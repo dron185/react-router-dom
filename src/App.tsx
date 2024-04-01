637715889fd2d4f15e5cbf06d9c5fa6881b23b64
Andrei Huryn <gurin.nord@gmail.com>
2024-04-01 13:07:20 +0300

    add useParams + find

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,56 @@
 import React, {useState} from 'react';
-import styles from "./components/Site.module.css";
-import {PageOne} from "./components/pages/PageOne";
-import {PageTwo} from "./components/pages/PageTwo";
-import {PageThree} from "./components/pages/PageThree";
+import {Adidas} from "./components/pages/Adidas";
+import {Puma} from "./components/pages/Puma";
+import {Abibas} from "./components/pages/Abibas";
 import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from "./components/pages/Error404";
+import {S} from "./components/pages/_styles"
+import {Model} from "./components/pages/Model";
+import { Model1 } from './components/pages/Model1';
 
-
+export const PATH = {
+    PAGE1: '/adidas',
+    PAGE2: '/puma',
+    PAGE3: '/abibas',
+    PAGEERROR: '/error404',
+    MODEL: '/adidas/:id',
+    MODEL1: '/puma/:id',
+} as const;
 
 function App() {
     return (
         <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    <div><NavLink to={"/page1"} className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>Page1</NavLink></div>
-                    <div><NavLink to={"/page2"} className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>Page2</NavLink></div>
-                    <div><NavLink to={"/page3"} className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>Page3</NavLink></div>
-                    {/*<NavLink*/}
-                    {/*    to="/messages"*/}
-                    {/*    className={({ isActive }) =>*/}
-                    {/*        isActive ? "activeNavLink" : "navLink"*/}
-                    {/*    }*/}
-                    {/*>*/}
-                    {/*    Messages*/}
-                    {/*</NavLink>*/}
-                </div>
-                <div className={styles.content}>
+            <S.Header><h1>HEADER</h1></S.Header>
+            <S.Body>
+                <S.Nav>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1} >Adidas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2} >Puma</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3} >Abibas</NavLink></S.NavWrapper>
+                </S.Nav>
+                <S.Content>
                     <Routes>
-                        <Route path="/" element={<Navigate to={"/page1"}/>}/>
+                        <Route path="/" element={<Navigate to={"/adidas"}/>}/>
 
-                        <Route path="/page1" element={<PageOne/>}/>
-                        <Route path="/page2" element={<PageTwo/>}/>
-                        <Route path="/page3" element={<PageThree/>}/>
+                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
+                        <Route path={PATH.PAGE2} element={<Puma/>}/>
+                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
+                        <Route path={PATH.MODEL} element={<Model/>}/>
+                        <Route path={PATH.MODEL1} element={<Model1/>}/>
 
-                        <Route path="/error404" element={<Error404/>}/>
-                        <Route path="/*" element={<Navigate to={"/error404"}/>}/>
+                        {/*<Route path={PATH.PAGEERROR} element={<Error404/>}/>*/}
+                        {/*<Route path="/*" element={<Navigate to={"/error404"}/>}/>*/}
+
+                        <Route path='/*' element={<Error404/>}/>
+                        {/*<Route path='/adidas/*' element={<Error404/>}/>*/}
                         // * - значит- все адреса, кроме тех, которые уже перечислили
 
                     </Routes>
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
+                </S.Content>
+            </S.Body>
+            <S.Footer>abibas 2023</S.Footer>
         </div>
     );
 }
 
-
 export default App;
+
