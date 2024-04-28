f838840a035fca0c1336c229db586b09f74691d4
Andrei Huryn <gurin.nord@gmail.com>
2024-04-28 17:05:16 +0300

    feat: add PATH

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import {PageThree} from "./components/pages/PageThree";
 import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from "./components/pages/Error404";
 
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+    ERROR: '/page/error'
+} as const;
 
 function App() {
     return (
@@ -14,28 +20,29 @@ function App() {
             <div className={styles.body}>
                 <div className={styles.nav}>
                     <div><NavLink
-                        to={"/page1"}
+                        to={PATH.PAGE1}
                         className={({ isActive}) => isActive ? styles.activeNavLink : styles.navLink}
                     >Page1</NavLink></div>
                     <div><NavLink
-                        to={"/page2"}
+                        to={PATH.PAGE2}
                         className={({ isActive}) => isActive ? styles.activeNavLink : styles.navLink}
                     >Page2</NavLink></div>
                     <div><NavLink
-                        to={"/page3"}
+                        to={PATH.PAGE3}
                         className={({ isActive}) => isActive ? styles.activeNavLink : styles.navLink}
                     >Page3</NavLink></div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
-                        <Route path="/" element={<Navigate to={"/page1"} />} />
+                        <Route path="/" element={<Navigate to={"/page1"}/>}/>
 
-                        <Route path="/page1" element={<PageOne />} />
-                        <Route path="/page2" element={<PageTwo/>} />
-                        <Route path="/page3" element={<PageThree/>} />
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
 
-                        <Route path={"/error404"} element={<Error404/>} />
-                        <Route path={"/*"} element={<Navigate to={"/error404"}/>} />
+                        <Route path={"/*"} element={<Error404/>}/>
+                        {/*<Route path={PATH.ERROR} element={<Error404/>}/>*/}
+                        {/*<Route path={"/*"} element={<Navigate to={PATH.ERROR}/>}/>*/}
                     </Routes>
                 </div>
             </div>
@@ -43,6 +50,5 @@ function App() {
         </div>
     );
 }
-
 
 export default App;
